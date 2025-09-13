c87a7691139efbc1d03a5a33ff38caf1502548ff
DjohnRussell <russelldanieljohn@gmail.com>
2025-09-13 21:25:10 +0200

    Updated the gitignore file and added the spring boot project

---
+++ FishApi/src/main/java/no/hiof/danieljr/FishApi/FishApiApplication.java
@@ -0,0 +1,13 @@
+package no.hiof.danieljr.FishApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishApiApplication.class, args);
+	}
+
+}
---
+++ FishApi/src/test/java/no/hiof/danieljr/FishApi/FishApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hiof.danieljr.FishApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
